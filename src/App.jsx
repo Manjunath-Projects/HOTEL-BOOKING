bb90e69172a0f0a6cb323d7eb3d35ddcd774c13b
MANJUNATH PATIL <patilmanju4321@gmail.com>
2025-05-20 13:32:06 +0530

    home page and home text's

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Hero from './Components/Hero';
 
 const App = () => {
+
+   const isOwnerPath=useLocation().pathname.includes("owner");
+
   return (
     <div>
-      <Navbar />
+     {!isOwnerPath && <Navbar />}
+     <div className='min-h-[70vh]'>
+      <Routes>
+        <Route path='/' element={<Hero/>} />
+      </Routes>
+
+     </div>
     </div>
   )
 }
